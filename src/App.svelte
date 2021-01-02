01497d90a21acb9b9b63c588bc34f74784943524
Chi-Toan Luu <toantoekan@gmail.com>
2021-01-02 06:51:44 +0100

    Init ToANote

---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    export let name: string;
+    import Profile from './Components/Profile.svelte'
+    import Todos from './Components/Todos.svelte';
+    import {auth, googleProvider} from './firebase';
+    import {authState} from 'rxfire/auth';
+
+    let user;
+    const unsubscribe = authState(auth).subscribe(u => user = u);
+
+    function login() {
+        auth.signInWithPopup(googleProvider);
+    }
+</script>
+
+<main>
+    <h1>Hello {name}!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
+
+<section>
+    {#if user}
+        <Profile {...user}/>
+        <button on:click={ () => auth.signOut() }>Logout</button>
+        <hr>
+        <Todos uid={user.uid}/>
+    {:else}
+        <button on:click={login}>
+            Signin with Google
+        </button>
+    {/if}
+</section>
